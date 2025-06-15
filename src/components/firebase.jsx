0f1428742e9e8a9b995322ae2efbaf00322b1cca
Singo <110606149+singoloua@users.noreply.github.com>
2025-06-15 22:36:28 +0100

    Update firebase.jsx

--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCvX6EmdsAu14qZT6CRPtgYXnbeBroMJMc",
-    authDomain: "inbox-5b352.firebaseapp.com",
-    projectId: "inbox-5b352",
-    storageBucket: "inbox-5b352.appspot.com",
-    messagingSenderId: "197329593994",
-    appId: "1:197329593994:web:70ea2ba8ea1ee7b6e6ef73",
-    measurementId: "G-DDN1RB9KQ0"
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
   };
 
 // Initialize Firebase
